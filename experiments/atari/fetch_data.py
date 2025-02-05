765efbf5d4f29732b22f9245ca60177afa10b308
yogesh1q2w <yogesh120699@gmail.com>
2025-02-05 07:40:45 +0100

    fetch data changes

--- experiments/atari/fetch_data.py
+++ experiments/atari/fetch_data.py
@@ -8,8 +8,11 @@ from slimfqi.sample_collection.samplers import UniformSamplingDistribution
 STORE_FILENAME_PREFIX = '$store$_'
 ELEMS = ['observation', 'action', 'reward', 'terminal']
 
-GAME='Pong'
-seed=5
+GAME=input("Enter game name: ")
+seed=input("Enter seed: ")
+seed=int(seed)
+source_data_dir = input("Enter source data directory: ")
+destination_data_dir = input("Enter destination data directory: ")
 
 
 def transform_single_checkpoint(idx_checkpoint, data_dir):
@@ -27,7 +30,7 @@ def transform_single_checkpoint(idx_checkpoint, data_dir):
                     stack_size=4,
                     update_horizon=1,
                     gamma=0.99,
-                    checkpoint_duration=1,
+                    checkpoint_duration=51,
                     compress=True)
     
     for observation, action, reward, terminal in zip(data["observation"], data["action"], data["reward"], data["terminal"]):
@@ -35,13 +38,14 @@ def transform_single_checkpoint(idx_checkpoint, data_dir):
         
     return rb
     
-data_dir = f'/home/stud_tripathi/rl/dqn_dataset/{GAME}/{seed}/replay_logs'
-destination_dir = f'/home/stud_tripathi/rl/slimdqn_dataset/{GAME}/{seed}'
+data_dir = f'{source_data_dir}/{GAME}/{seed}/replay_logs'
+destination_dir = f'{destination_data_dir}/{GAME}/{seed}'
 os.makedirs(destination_dir)
 for idx_checkpoint in range(0, 50):
-    
-    rb = transform_single_checkpoint(idx_checkpoint, data_dir)
-    rb.save(checkpoint_dir=destination_dir, iteration_number=idx_checkpoint)
-    del rb
-    print(f"---------Finished seed = {seed}, ckpt = {idx_checkpoint}---------")
-    
+    try:
+        rb = transform_single_checkpoint(idx_checkpoint, data_dir)
+        rb.save(checkpoint_dir=destination_dir, iteration_number=idx_checkpoint)
+        del rb
+        print(f"---------Finished seed = {seed}, ckpt = {idx_checkpoint}---------")
+    except Exception as e:
+        print(f"---------FAILED for seed = {seed}, ckpt = {idx_checkpoint}, error: {e}---------")
