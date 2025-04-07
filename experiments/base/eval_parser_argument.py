f7d4038fea41240ae55fc649eefdf3a1816b45eb
yogesh1q2w <yogesh120699@gmail.com>
2025-04-07 05:52:34 +0200

    eval args for car_on_hill

--- experiments/base/eval_parser_argument.py
+++ experiments/base/eval_parser_argument.py
@@ -35,14 +35,14 @@ def add_eval_arguments(parser: argparse.ArgumentParser):
         "--horizon",
         help="Horizon for truncation.",
         type=int,
-        default=27_000,
+        default=100,
     )
     parser.add_argument(
         "-nespi",
         "--n_evaluation_steps_per_epoch",
         help="Evaluation steps per epoch.",
         type=int,
-        default=125_000,
+        default=1_000,
     )
     parser.add_argument(
         "-ee",
