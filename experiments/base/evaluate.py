c39db87dceecb756cbc0f596e001d849ec4cd98c
yogesh1q2w <yogesh120699@gmail.com>
2025-04-07 03:57:58 +0200

    separate p and args in evaluation

--- experiments/base/evaluate.py
+++ experiments/base/evaluate.py
@@ -20,7 +20,7 @@ def select_action(best_action_fn, params, state, key, n_actions, epsilon_eval):
     )
 
 
-def evaluate_one_epoch(key, agent_best_action, agent_params, env, p):
+def evaluate_one_epoch(key, agent_best_action, agent_params, env, args):
     eval_episode_returns_epoch = []
     eval_episode_lengths_epoch = []
     eval_episode_returns_epoch.append(0)
@@ -30,7 +30,7 @@ def evaluate_one_epoch(key, agent_best_action, agent_params, env, p):
     env.reset()
     has_reset = False
 
-    while n_evaluation_steps < p["n_evaluation_steps_per_epoch"] or not has_reset:
+    while n_evaluation_steps < args.n_evaluation_steps_per_epoch or not has_reset:
         key, action_key = jax.random.split(key)
         action = select_action(
             agent_best_action,
@@ -38,7 +38,7 @@ def evaluate_one_epoch(key, agent_best_action, agent_params, env, p):
             env.state,
             action_key,
             env.n_actions,
-            p["epsilon_eval"],
+            args.epsilon_eval,
         ).item()
 
         reward, absorbing = env.step(action)
@@ -47,38 +47,38 @@ def evaluate_one_epoch(key, agent_best_action, agent_params, env, p):
         eval_episode_returns_epoch[-1] += reward
         eval_episode_lengths_epoch[-1] += 1
 
-        has_reset = absorbing or env.n_steps >= p["horizon"]
+        has_reset = absorbing or env.n_steps >= args.horizon
         if has_reset:
             env.reset()
 
-        if has_reset and n_evaluation_steps < p["n_evaluation_steps_per_epoch"]:
+        if has_reset and n_evaluation_steps < args.n_evaluation_steps_per_epoch:
             eval_episode_returns_epoch.append(0)
             eval_episode_lengths_epoch.append(0)
 
     return eval_episode_returns_epoch, eval_episode_lengths_epoch
 
 
-def evaluate(p, env):
-    if p["algo_name"] == "dqn":
+def evaluate(p, args, env):
+    if args.algo_name == "dqn":
         agent = DQN(
             key=jax.random.PRNGKey(0),
             observation_dim=env.observation_shape,
             n_actions=env.n_actions,
-            features=p["features"],
-            architecture_type=p["architecture_type"],
+            features=p["shared_parameters"]["features"],
+            architecture_type=p["shared_parameters"]["architecture_type"],
             learning_rate=-1,
             gamma=-1,
             update_horizon=-1,
             target_update_frequency=-1,
             adam_eps=-1,
         )
-    elif p["algo_name"] == "cql":
+    elif args.algo_name == "cql":
         agent = CQL(
             key=jax.random.PRNGKey(0),
             observation_dim=env.observation_shape,
             n_actions=env.n_actions,
-            features=p["features"],
-            architecture_type=p["architecture_type"],
+            features=p["shared_parameters"]["features"],
+            architecture_type=p["shared_parameters"]["architecture_type"],
             learning_rate=-1,
             gamma=-1,
             update_horizon=-1,
@@ -87,29 +87,29 @@ def evaluate(p, env):
             alpha_cql=-1,
         )
     else:
-        assert False, f"Invalid algorithm {p['algo_name']}"
+        assert False, f"Invalid algorithm {args.algo_name}"
 
     eval_episode_returns, eval_episode_lengths = evaluate_one_epoch(
-        jax.random.PRNGKey(seed=p["seed"]),
+        jax.random.PRNGKey(seed=args.seed),
         agent.best_action,
         pickle.load(
             open(
-                f"{p['save_path']}/models/{p['seed']}/{p['epoch']}",
+                f"{p['save_path']}/models/{args.seed}/{args.epoch}",
                 "rb",
             )
         )["params"],
         env,
-        p,
+        args,
     )
 
     os.makedirs(
-        f"{p['save_path']}/episode_returns_and_lengths/{p['seed']}",
+        f"{p['save_path']}/episode_returns_and_lengths/{args.seed}",
         exist_ok=True,
     )
     json.dump(
         {"episode_returns": list(eval_episode_returns), "episode_lengths": list(eval_episode_lengths)},
         open(
-            f"{p['save_path']}/episode_returns_and_lengths/{p['seed']}/{p['epoch']}_results.json",
+            f"{p['save_path']}/episode_returns_and_lengths/{args.seed}/{args.epoch}_results.json",
             "w",
         ),
     )
--- experiments/car_on_hill/evaluate.py
+++ experiments/car_on_hill/evaluate.py
@@ -16,14 +16,13 @@ def run(argvs=sys.argv[1:]):
 
     env_name = os.path.abspath(__file__).split("/")[-2]
     p = json.load(open(f"experiments/{env_name}/exp_output/{args.experiment_name}/parameters.json", "rb"))
-    p = dict(list(p["shared_parameters"].items()) + list(p[args.algo_name].items()) + list(vars(args).items()))
     p["save_path"] = os.path.join(
         os.path.dirname(os.path.abspath(__file__)),
-        f"../{env_name}/exp_output/{p['experiment_name']}/{p['algo_name']}",
+        f"../{env_name}/exp_output/{args.experiment_name}/{args.algo_name}",
     )
     env = CarOnHill()
 
-    evaluate(p, env)
+    evaluate(p, args, env)
 
 
 if __name__ == "__main__":
