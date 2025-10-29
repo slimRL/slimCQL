ba35c24317f0d8fd467f4c256ca23656c8792e4e
yogesh1q2w <yogesh120699@gmail.com>
2025-10-29 09:19:07 +0100

    clean data prep

---
+++ experiments/atari/prepare_replay_buffers.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+import gzip
+from tqdm import tqdm
+import numpy as np
+
+from slimdqn.sample_collection.replay_buffer import ReplayBuffer
+from slimdqn.sample_collection.samplers import Uniform
+
+
+GAME = "VideoPinball"
+RUN = 1
+SRC_DIR = "experiments/atari/datasets/numpy_dataset"
+DEST_DIR = "experiments/atari/datasets/slim_dataset"
+
+
+os.makedirs(f"{DEST_DIR}/{GAME}/{RUN}")
+
+for ckpt in tqdm(range(0, 50)):
+    os.makedirs(f"{DEST_DIR}/{GAME}/{RUN}/{ckpt}", exist_ok=True)
+    shutil.copyfile(f"{SRC_DIR}/{GAME}/{RUN}/observations_{ckpt}.gz", f"{DEST_DIR}/{GAME}/{RUN}/{ckpt}/observations.gz")
+
+    arrays = {}
+    for attr in ["observations", "actions", "rewards", "is_terminals", "episode_ends"]:
+        arrays[attr] = np.load(gzip.GzipFile(fileobj=open(f"{SRC_DIR}/{GAME}/{RUN}/{attr}_{ckpt}.gz", "rb")))
+
+    # Change replay buffer variables here for different `update_horizon`/`gamma`
+    rb = ReplayBuffer(
+        sampling_distribution=Uniform(RUN),
+        max_capacity=1_000_000,
+        batch_size=32,
+        stack_size=4,
+        update_horizon=1,
+        gamma=0.99,
+        clipping=None,
+    )
+    rb.save(
+        f"{DEST_DIR}/{GAME}/{RUN}",
+        ckpt,
+        arrays["actions"],
+        arrays["rewards"],
+        arrays["is_terminals"],
+        arrays["episode_ends"],
+    )
+    del rb
+    print(f"---------Game = {GAME}; Finished seed = {RUN}, ckpt = {ckpt}---------", flush=True)
