19206aa0802f47daf377c1fa95a3db0f47ff2eb5
yogesh1q2w <yogesh120699@gmail.com>
2025-10-29 11:28:25 +0100

    launch files

--- launch_job/atari/train.sh
+++ launch_job/atari/train.sh
@@ -9,7 +9,7 @@ export XLA_PYTHON_CLIENT_MEM_FRACTION=$(echo "scale=2 ; 0.98 / ($LAST_SEED - $FI
 
 for (( seed=$FIRST_SEED; seed<=$LAST_SEED; seed++ ))
 do
-    python3 experiments/$ENV_NAME/$ALGO_NAME.py --experiment_name $EXPERIMENT_NAME --seed $seed $ARGS &> experiments/$ENV_NAME/logs/$EXPERIMENT_NAME/$ALGO_NAME/seed_$seed.out & 
+    python3 experiments/$ENV_NAME/$ALGO_NAME.py --experiment_name $EXPERIMENT_NAME --seed $seed $ARGS >> experiments/$ENV_NAME/logs/$EXPERIMENT_NAME/$ALGO_NAME/seed_$seed.out 2>&1 &
 done
 wait
 
@@ -24,7 +24,7 @@ do
     epoch=0
     while (( epoch < N_EPOCHS+1 ))
     do
-        python3 experiments/$ENV_NAME/evaluate.py --experiment_name $EXPERIMENT_NAME --algo_name $ALGO_NAME --seed $seed --epoch $epoch --horizon 27_000 --n_evaluation_steps_per_epoch 125_000 >> experiments/$ENV_NAME/logs/$EXPERIMENT_NAME/$ALGO_NAME/seed_$seed.out 2>&1 &
+        python3 experiments/$ENV_NAME/evaluate.py --experiment_name $EXPERIMENT_NAME --algo_name $ALGO_NAME --seed $seed --epoch $epoch >> experiments/$ENV_NAME/logs/$EXPERIMENT_NAME/$ALGO_NAME/seed_$seed.out 2>&1 &
         
         ((epoch++))
         
@@ -33,6 +33,6 @@ do
         fi
     done
     wait
-    python3 experiments/$ENV_NAME/synchronize_evaluation_wandb.py --experiment_name $EXPERIMENT_NAME --algo_name $ALGO_NAME --seed $seed --epoch $epoch --delete_models  >> experiments/$ENV_NAME/logs/$EXPERIMENT_NAME/$ALGO_NAME/seed_$seed.out 2>&1 &
+    python3 experiments/$ENV_NAME/synchronize_evaluation_wandb.py --experiment_name $EXPERIMENT_NAME --algo_name $ALGO_NAME --seed $seed --env_name "atari" --delete_models >> experiments/$ENV_NAME/logs/$EXPERIMENT_NAME/$ALGO_NAME/seed_$seed.out 2>&1 &
 done
 wait
